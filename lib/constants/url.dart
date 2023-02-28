c6f8d8ad7250a99882dd66b4190ff6e1d651ec21
Francis KOUAHO <kouahofrancis@gmail.com>
2023-02-28 13:33:47 +0100

    add scan

--- lib/constants/url.dart
+++ lib/constants/url.dart
@@ -1,13 +1,13 @@
 import 'dart:io' show Platform;
 
 
-class AppUrl {
+/*class AppUrl {
   static String baseUrl = Platform.isIOS ? 'http://127.0.0.1:3333/api/v1' : 'http://10.0.2.2:3333/api/v1';
   static String baseUrlSocket = Platform.isIOS ? 'http://127.0.0.1:3333' : 'http://10.0.2.2:3333';
-}
+}*/
 
 
-/*class AppUrl {
+class AppUrl {
   static String baseUrl = 'https://api.letsgoeurope.fr/api/v1';
   static String baseUrlSocket = 'https://api.letsgoeurope.fr/';
-}*/
+}
